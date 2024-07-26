910a48f6bf33cb1835afab52a1bac36bd196b9cd
naufinur <khayatinaufi23gmail.com>
2024-07-26 19:57:35 +0700

    ⚙: fix cors

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://grobak-fe.vercel.app" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://grobak-fe.vercel.app",
+    // origin: "http://localhost:5174",
+  })
+);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
